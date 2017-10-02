be4ce9d05d3bbf881630acf43c6f27688bdf060b
Ciprian Hacman <ciprian.hacman@sematext.com>
2017-10-02 18:30:29 +0300

    Update cleanup logic

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+rm -r site/*
 mkdocs build
 cp site/assets/javascripts/application-*.js site/assets/javascripts/application.js
-rm -r site/* /var/www/docs/*
+rm -r /var/www/docs/*
 cp -r site/* /var/www/docs/
