612ffb9948ff4a916285148406e1c59f5bd90bd7
megastef <s.thies@bigdata-analyst.de>
2018-03-13 09:43:08 +0100

    remove cp command for dynamic script names from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -r site/*
 mkdocs build
-cp site/assets/javascripts/application-*.js site/assets/javascripts/application.js
+# cp site/assets/javascripts/application-*.js site/assets/javascripts/application.js
 rm -r /var/www/docs/*
 cp -r site/* /var/www/docs/
