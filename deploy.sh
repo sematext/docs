e762fad7d3f43435bdc9a411f3bbec9a97b4e7ec
Stefan Thies <s.thies@bigdata-analyst.de>
2018-11-26 20:44:32 +0100

    Add --strict option to mkdocs

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -r site/*
-mkdocs build
+mkdocs build --strict
 # cp site/assets/javascripts/application-*.js site/assets/javascripts/application.js
 rm -r /var/www/docs/*
 cp -r site/* /var/www/docs/
