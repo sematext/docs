bb3c003749e257ccef9c26e43e0cac8a0d8f82c2
Otis Gospodnetić <otis@sematext.com>
2019-04-13 11:23:11 -0400

    Removed a trace of ActOn

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -17,10 +17,6 @@ setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent
 })();
 /* End Inspectlet Embed Code */
 
-/* begin act-on code */
-/*<![CDATA[*/(function(w,a,b,d,s){w[a]=w[a]||{};w[a][b]=w[a][b]||{q:[],track:function(r,e,t){this.q.push({r:r,e:e,t:t||+new Date});}};var e=d.createElement(s);var f=d.getElementsByTagName(s)[0];e.async=1;e.src='//marketing.sematext.com/cdnr/96/acton/bn/tracker/29440';f.parentNode.insertBefore(e,f);})(window,'ActOn','Beacon',document,'script');ActOn.Beacon.track();/*]]>*/
-/* end act-on code */
-
 /* begin GA code */
     (function(i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
