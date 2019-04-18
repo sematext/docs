614327209c5d45b817d7eb191b5284e4f59988cc
Jorge Moreno <alterebro@gmail.com>
2019-04-18 17:08:04 +0200

    IN-4624 Fix GTM on /docs

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -32,13 +32,6 @@ setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent
     ga('create', 'UA-2365268-3', 'auto');
 /* end GA code */
 
-/* Google Tag Manager */
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K47WKMR');
-/* End Google Tag Manager */
 
 /* Google Material Design Lite JS -  https://getmdl.io  */
 
