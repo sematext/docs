4dd9945fef801923af3c934a238129a4daa46756
hasan <hasan_wajahat@hotmail.com>
2023-06-28 18:33:17 +0500

    SC-16634 Fixes wiki example puppeteer script

--- docs/synthetics/puppeteer-scripts/login-wiki.js
+++ docs/synthetics/puppeteer-scripts/login-wiki.js
@@ -1,25 +1,26 @@
 const assert = require('assert');
 
 async function testPage(page) {
+    const assert = require('assert');
     await page.goto('https://en.wikipedia.org/');
     await Promise.all([
         page.waitForNavigation(),
-        page.click('#pt-login'),
+        page.click('#pt-login-2'),
     ]);
-    const userName = '<username>';
+    const userName = 'Sematext-wiki';
     await page.type('#wpName1', userName);
-    await page.type('#wpPassword1', '<password>');
+    await page.type('#wpPassword1', 'semapassword');
     await Promise.all([
         page.waitForNavigation(),
         page.click('#wpLoginAttempt'),
     ]);
     await Promise.all([
         page.waitForNavigation(),
-        page.click('#pt-userpage'),
+        page.click('#pt-userpage-2'),
     ]);
     const userNameFromPage = await page.evaluate(() => document.querySelector('#firstHeading').textContent);
     console.log(userNameFromPage);
-    assert.equal(userNameFromPage.split(':')[1].toLowerCase(), userName.toLowerCase());
+    assert.ok(userNameFromPage.split(',')[1].toLowerCase().includes(userName.toLowerCase()));
 }
 
 module.exports = testPage;
