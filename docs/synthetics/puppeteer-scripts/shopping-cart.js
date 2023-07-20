a49c7c5fc14980d3a504fb4d3953abdf5be20da1
Haris Salman <haris.salman@sematext.com>
2023-07-20 12:59:20 +0200

    Fix Browser monitor UJS shopping cart example

--- docs/synthetics/puppeteer-scripts/shopping-cart.js
+++ docs/synthetics/puppeteer-scripts/shopping-cart.js
@@ -1,23 +1,26 @@
-async function testPage(page) {
+async function testPage(page) {  
     await page.goto('https://abookapart.com/products/cross-cultural-design');
     await page.waitForSelector('.buy-option input');
     await page.click('.buy-option input');
     await page.click('.buy-options__btn');
     await page.waitForSelector('.icartCheckoutBtn');
     await page.click('.icartCheckoutBtn');
+
+    // Fill out the form
     await page.waitForSelector('#email');
-    await page.type('#email', 'john@reddiff.com');
+    await page.type('#email', 'john@example.com');
     await page.type('[name="firstName"]', 'John');
-    await page.type('[name="lastName"]', 'Adams');
+    await page.type('[name="lastName"]', 'Smith');
     await page.type('[name="address1"]', '52, Sunset Blvd');
     await page.type('[name="city"]', 'Los Angeles');
+    await page.select('[id="Select1"]', 'CA');
     await page.type('[name="postalCode"]', '90001');
-    await page.type('[name="phone"]', '5551234567');
+    await page.type('[name="phone"]', '16468955093');
     await Promise.all([
       page.waitForNavigation(),
       page.click('button[type="submit"]'),
     ]);
-    await page.screenshot({ path: 'shoppingcart.jpg' });
+    await page.screenshot({ path: 'shoppingCart.jpg' });
 }
 
 module.exports = testPage;
