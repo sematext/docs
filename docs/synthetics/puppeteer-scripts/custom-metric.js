4edbee59d1a978ea4029184d9ffd48694e50ecc3
hasan <hasan_wajahat@hotmail.com>
2023-09-20 11:14:58 +0500

    Fixes typo

--- docs/synthetics/puppeteer-scripts/custom-metric.js
+++ docs/synthetics/puppeteer-scripts/custom-metric.js
@@ -10,7 +10,7 @@ async function testPage(page, context) {
     const usedHeap = performanceMetrics.metrics.find((x) => x.name === 'JSHeapUsedSize').value;
 
     // Define the chosen metrics as custom metrics in Sematext Synthetics
-    // Metric names should be unique for this Synthetics application
+    // Metric names should be unique for this Synthetics App
     // Metric names should be alphanumeric, otherwise they will be ignored
     context.setMetric('heap.size', usedHeap);
     context.setMetric('script.time', scriptDuration);
