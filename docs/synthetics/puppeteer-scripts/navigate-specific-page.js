43049723d5b35d761640690803d5b142732bd8d0
Haris Salman <haris.salman@sematext.com>
2023-12-05 15:48:21 +0100

    SC-17976 Improve formatting

--- docs/synthetics/puppeteer-scripts/navigate-specific-page.js
+++ docs/synthetics/puppeteer-scripts/navigate-specific-page.js
@@ -2,18 +2,19 @@ const assert = require('assert');
 
 async function testPage(page) {
     await page.setViewport({ width: 1280, height: 800 });
-    // visit the home page
+    // Visit the home page
     await page.goto('https://www.apple.com/');
-    // navigate to Music page. You can use selectors to click
-    // any element. For more info refer to https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors
+
+    // Navigate to Music page. You can use selectors to click any element.
+    // For more info refer to https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors
     await Promise.all([
         page.waitForNavigation(),
         page.click('[href="/mac/"]'),
     ]);
     const title = await page.title();
     console.log(title);
-    // verify the title
-  
+
+    // Verify the title
     assert.equal(title, 'Mac - Apple');
     await page.screenshot({ path: 'screenshot.jpg' });
 }
