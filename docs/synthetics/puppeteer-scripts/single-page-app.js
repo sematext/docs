969840787d6922f4b7d50245954f550e8a4e1db0
hasan <hasan_wajahat@hotmail.com>
2024-05-02 17:56:25 +0500

    Improves some of the example scripts

--- docs/synthetics/puppeteer-scripts/single-page-app.js
+++ docs/synthetics/puppeteer-scripts/single-page-app.js
@@ -5,23 +5,22 @@ async function testPage(page) {
         height: 800,
     })
 
-    // Go to the URL which switches into the demo account and wait for the redirect to finish
-    await page.goto('https://apps.sematext.com/demo');
-    await page.waitForTimeout(3000);
+    // Go to the URL which switches into the demo account and wait for domcontentloaded
+    await page.goto('https://apps.sematext.com/demo', { waitUntil: 'domcontentloaded' });
 
-    const SYNTHETICS_APP_SELECTOR = 'a[href="/ui/synthetics"]';
+    const SYNTHETICS_APP_SELECTOR = 'a[href="/ui/synthetics/monitors"]';
     await page.waitForSelector(SYNTHETICS_APP_SELECTOR);
     await page.click(SYNTHETICS_APP_SELECTOR);
 
-    // Wait for the locations map to show up, then wait a bit more for it to load fully for the screenshot
+    // Wait for the locations map to show up, then take a screenshot
     await page.waitForSelector('[id="locationsMap"]');
-    await page.waitForTimeout(2500); // We only need this to load everything for the screenshot
     await page.screenshot({ path: 'synthetics.png' });
 
     // Ensure the sidebar element for navigating to the Infra page exists, then click it
     const INFRA_APP_SELECTOR = 'a[href="/ui/infrastructure"]';
     await page.waitForSelector(INFRA_APP_SELECTOR);
     await page.click(INFRA_APP_SELECTOR);
+    await page.screenshot({ path: '3_synthetics.png' });
 }
 
 module.exports = testPage
--- docs/synthetics/puppeteer-scripts/text-search.js
+++ docs/synthetics/puppeteer-scripts/text-search.js
@@ -1,6 +1,6 @@
 async function testPage(page) {
     await page.goto('https://sematext.com/');
-    await page.waitForXPath('//*[contains(., "Sematext provides Monitoring & Alerting")]');
+    await page.waitForSelector('text/Sematext provides Monitoring & Alerting');
     await page.screenshot({ path: 'screenshot.jpg' });
 }
 
